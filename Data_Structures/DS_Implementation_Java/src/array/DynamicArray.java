f9bcca1073764c125df5a6c28f6ffed82539112c
Adrien Tremblay <adrien@adrientremblay.com>
2020-08-21 15:16:27 -0400

    Initial DynamicArray class

---
+++ Data_Structures/DS_Implementation_Java/src/array/DynamicArray.java
@@ -0,0 +1,89 @@
+package array;
+
+import java.util.Arrays;
+
+public class DynamicArray<E> {
+    private int capacity;
+    private int length;
+    private E[] arr;
+
+    // Private methods
+    private void resize() {
+        this.capacity *= 2;
+        E[] newArr = (E[]) new Object[this.capacity];
+        for (int i = 0 ; i < arr.length ; i++){
+            newArr[i]= arr[i];
+        }
+        this.arr = newArr;
+    }
+
+    // Constructors
+    public DynamicArray() {
+        this(8);
+    }
+
+    public DynamicArray(int capacity){
+        if (capacity < 0) return;
+
+        this.capacity=capacity;
+        this.length=0;
+        arr = (E[]) new Object[capacity];
+    }
+
+    // Public Methods
+    public E get(int index) {
+        return arr[index];
+    }
+
+    public void insert(int index, E value) {
+        if (index < 0) return;
+
+        // index will resolve to being the end of the array
+        if (index >= this.length) {
+            this.append(value);
+            return;
+        }
+
+        if  (this.length == capacity - 1) resize();
+
+        for (int i = this.length - 1 ; i >= index ; i--) {
+           arr[i+1]  = arr[i];
+        }
+        arr[index] = value;
+    }
+
+    public void append(E value) {
+        if  (this.length == capacity - 1) resize();
+
+        arr[this.length] = value;
+        this.length++;
+    }
+
+    public int search(E value) {
+        for (int i = 0 ; i < this.length ; i++) {
+           if (arr[i].equals(value)) return i;
+        }
+
+        return -1;
+    }
+
+    public void delete(E value) {
+        int deleteIndex = this.search(value);
+        if (deleteIndex == -1) return;
+
+        for (int i = deleteIndex ; i < this.length ; i++){
+           arr[i] = arr[i+1];
+        }
+
+        this.length-=1;
+    }
+
+    @Override
+    public String toString() {
+        return "DynamicArray{" +
+                "capacity=" + capacity +
+                ", length=" + length +
+                ", arr=" + Arrays.toString(arr) +
+                '}';
+    }
+}
--- Data_Structures/DS_Implementation_Java/src/com/company/Main.java
+++ Data_Structures/DS_Implementation_Java/src/com/company/Main.java
@@ -1,15 +1,19 @@
 package com.company;
 
+import array.DynamicArray;
+
 import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
-        String message = "Hello World";
+        DynamicArray<Integer> testDA = new DynamicArray<Integer>();
+        testDA.append(1);
+        testDA.append(2);
+        testDA.append(3);
 
-        Date today = new Date();
+        testDA.delete(2);
 
-
-        System.out.println(message);
+        System.out.println(testDA);
     }
 }
