e4f349cd640127065023f4f99a6734db9a6d1d0f
Adrien Tremblay <adrien@adrientremblay.com>
2020-08-21 14:18:52 -0400

    Adding Implementation repo

---
+++ Data_Structures/DS_Implementation_Java/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String message = "Hello World";
+
+        Date today = new Date();
+
+
+        System.out.println(message);
+    }
+}
