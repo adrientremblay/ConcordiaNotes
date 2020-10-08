8a5cd0fa1fd3c746dd36b3a1fed3a609c1562be7
Adrien Tremblay <adrien@adrientremblay.com>
2020-10-08 13:03:56 -0400

    print everything

--- Principles_Of_Programming_Languages(COMP348)/lisp/notes.lisp
+++ Principles_Of_Programming_Languages(COMP348)/lisp/notes.lisp
@@ -1,23 +1,23 @@
-; lists
+(print `lists)
 (print `()) ; empty list
 (print  `(1 3 5 7)) ; list of four elements
 (print `((1 2) (3 4))) ; list of two elements: two other lists
 (print `(((1 2) (3 4)))) ; list of one element which is a list with two lists inside it
-;cons
-(print (cons 'a (cons (cons 'b (cons 'c ())) (cons 'd (cons 'e ())))))
-(print (cons (+ 2 3) '(b c)))
-(print (cons (+ 2 3) '(b c)))
+(print `cons)
+(print (cons `a (cons (cons `b (cons `c ())) (cons `d (cons `e ())))))
+(print (cons (+ 2 3) `(b c)))
+(print (cons `(+ 2 3) `(b c)))
 
-; expressions
+(print `expressions)
 ; (function-name arguments)
-(- 14 (* 2 3)) ; 14 - 2 * 3
-(+ 1 2 3 4) ; 1 + 2 + 3 + 4
-(< 1 3 2) ; 1 < 3 and 3 < 1
+(print (- 14 (* 2 3))) ; 14 - 2 * 3
+(print (+ 1 2 3 4)) ; 1 + 2 + 3 + 4
+(print (< 1 3 2)) ; 1 < 3 and 3 < 1
 ; prohibiting expression evaluation
-(/ (* 2 6) 3) ; returns 4
-'(/ (* 2 6) 3) ; returns (/ (* 2 6) 3)
+(print (/ (* 2 6) 3)) ; returns 4
+(print `(/ (* 2 6) 3)) ; returns (/ (* 2 6) 3)
 
-; boolean operations
+(print `boolean_operators)
 ; and or not
 ; prexix notation
 (let ((x 5))
