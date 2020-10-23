30b74d4cc6d4380999fc9ad62dca994c9990c445
Adrien Tremblay <adrien@adrientremblay.com>
2020-10-23 00:04:03 -0400

    Print response based on comparison of guess and secret number

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -6,17 +7,27 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("Psst... The secret number is: {}.", secret_number);
 
     println!("Please input your guess.!");
 
     let mut guess = String::new();
 
-    println!(
-        "Bytes used: {}",
+    // println!(
+    //     "Bytes used: {}",
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line"),
-    );
+    // );
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too Big!"),
+        Ordering::Equal => println!("You Win!"),
+    }
+
+    println!("The secret number was: {}.", secret_number);
 }
