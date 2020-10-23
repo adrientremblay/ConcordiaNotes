0ae76d25dd1f54e69fbe8c614aebb9064daf40b1
Adrien Tremblay <adrien@adrientremblay.com>
2020-10-23 00:28:44 -0400

    Infinite loop for multiple guesses

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -7,26 +7,22 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
+    loop {
+        println!("Please input your guess:");
 
-    println!("Please input your guess.!");
-
-    let mut guess = String::new();
-
-    // println!(
-    //     "Bytes used: {}",
+        let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
-            .expect("Failed to read line"),
-    // );
+            .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("You guessed: {}", guess);
 
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too Big!"),
-        Ordering::Equal => println!("You Win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => println!("You Win!"),
+        }
     }
 
     println!("The secret number was: {}.", secret_number);
