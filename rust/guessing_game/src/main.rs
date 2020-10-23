f2c74c07d0524e23f4dc9f3f691a265514b65ea5
Adrien Tremblay <adrien@adrientremblay.com>
2020-10-23 00:52:29 -0400

    Improved game with guess range

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -3,12 +3,16 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
+    let mut min = 1;
+    let mut max = 100;
+
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(min, max + 1);
 
     loop {
         println!("Please input your guess:");
+        println!("Psst... The number is between {} and {}.", min, max);
 
         let mut guess = String::new();
         io::stdin()
@@ -22,8 +26,14 @@ fn main() {
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too Big!"),
+            Ordering::Less => {
+                println!("Too small!");
+                min = guess + 1;
+            }
+            Ordering::Greater => {
+                println!("Too Big!");
+                max = guess - 1;
+            }
             Ordering::Equal => {
                 println!("You Win!");
                 break;
