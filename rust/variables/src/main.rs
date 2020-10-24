faf2427d6d3dd273bf2975a83186c06078c2aaf7
Adrien Tremblay <adrien@adrientremblay.com>
2020-10-24 00:49:45 -0400

    variable shadowing

--- rust/variables/src/main.rs
+++ rust/variables/src/main.rs
@@ -1,8 +1,17 @@
 fn main() {
+    // mutable variable
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
 
+    // constant variable
     const MAX_POINTS: u32 = 100_000;
+    println!("Max Points: {}", MAX_POINTS);
+
+    // variable shadowing
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
 }
