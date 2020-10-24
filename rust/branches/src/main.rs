883dbab9a26d606b387ceefe3553a372369c4ca1
Adrien Tremblay <adrien@adrientremblay.com>
2020-10-24 10:45:43 -0400

    multiple conditions

--- rust/branches/src/main.rs
+++ rust/branches/src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
-    let number = 3;
+    let number = 7;
 
     if number < 5 {
         println!("condition was true");
     } else {
         println!("condition was false");
+    }
+
+    if number != 0 {
+        println!("number is not zero!");
+    } else if number % 3 == 0 {
+        println!("number is dibisible by 3!");
+    } else {
+        println!("number is boring...");
     }
 }
