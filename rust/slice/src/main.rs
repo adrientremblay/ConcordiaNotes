e5547020efd4c809986ccf55987bd952a3ae2b66
Adrien Tremblay <adrien@adrientremblay.com>
2020-10-25 18:30:08 -0400

    slice

--- rust/slice/src/main.rs
+++ rust/slice/src/main.rs
@@ -1,15 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("A slow dog ran over a big cat.");
+    println!("{}", first_word(&s));
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
