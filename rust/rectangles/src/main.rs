e1852df86575572c092140dc1ca86762ccc8f93f
Adrien Tremblay <adrien@adrientremblay.com>
2020-10-26 11:14:08 -0400

    struct methods

--- rust/rectangles/src/main.rs
+++ rust/rectangles/src/main.rs
@@ -4,16 +4,40 @@ struct Rectangle {
     height: u32,
 }
 
-fn main() {
-    let rect1 = Rectangle {
-        width: 5,
-        height: 12,
-    };
-
-    println!("The area of the triangle is {} pixels.", area(&rect1));
-    println!("{:#?}", rect1);
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.height > other.height && self.width > other.width
+    }
+    // methods assosiated to struct (essentially a static method)
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    let sq = Rectangle::square(5);
+
+    println!("The area of the triangle is {} pixels.", rect1.area());
+    println!("{:#?}", rect1);
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
