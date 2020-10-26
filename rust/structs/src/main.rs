43206c62307edbc4a0b6b1344131545fd4574791
Adrien Tremblay <adrien@adrientremblay.com>
2020-10-26 09:20:08 -0400

    change email

--- rust/structs/src/main.rs
+++ rust/structs/src/main.rs
@@ -1,12 +1,15 @@
 fn main() {
     println!("Hello, world!");
 
-    let user1 = User {
+    let mut user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("somerusername123"),
         active: true,
         sign_in_count: 1,
     };
+    println!("Email is: {}", user1.email);
+    user1.email = String::from("somoneelse@example.com");
+    println!("Email is: {}", user1.email);
 }
 
 struct User {
