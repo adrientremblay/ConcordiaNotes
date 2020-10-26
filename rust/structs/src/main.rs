fdf3b497a5e3805fd5096087aae8534faa9bd58d
Adrien Tremblay <adrien@adrientremblay.com>
2020-10-26 09:32:36 -0400

    stuff

--- rust/structs/src/main.rs
+++ rust/structs/src/main.rs
@@ -10,6 +10,30 @@ fn main() {
     println!("Email is: {}", user1.email);
     user1.email = String::from("somoneelse@example.com");
     println!("Email is: {}", user1.email);
+
+    let user2 = build_user(String::from("someone@example.com"), String::from("rustman"));
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        active: user1.active,
+        sign_in_count: user1.sign_in_count,
+    };
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        ..user1
+    };
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
 }
 
 struct User {
